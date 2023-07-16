dc7c91eb61ed4c27cb62333b0c0aa9d49b45490f
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-16 09:42:12 +0530

    Complete the implementation from the DB table creation and implementation

--- app/src/main/java/com/vinaylogics/roomdatabaseexample/bl/database/daos/SchoolDao.kt
+++ app/src/main/java/com/vinaylogics/roomdatabaseexample/bl/database/daos/SchoolDao.kt
@@ -8,7 +8,12 @@ import androidx.room.Transaction
 import com.vinaylogics.roomdatabaseexample.bl.database.entities.Director
 import com.vinaylogics.roomdatabaseexample.bl.database.entities.School
 import com.vinaylogics.roomdatabaseexample.bl.database.entities.Student
+import com.vinaylogics.roomdatabaseexample.bl.database.entities.Subject
 import com.vinaylogics.roomdatabaseexample.bl.database.entities.relations.SchoolAndDirector
+import com.vinaylogics.roomdatabaseexample.bl.database.entities.relations.SchoolWithStudents
+import com.vinaylogics.roomdatabaseexample.bl.database.entities.relations.StudentSubjectCrossRef
+import com.vinaylogics.roomdatabaseexample.bl.database.entities.relations.StudentWithSubjects
+import com.vinaylogics.roomdatabaseexample.bl.database.entities.relations.SubjectWithStudents
 
 @Dao
 interface SchoolDao {
@@ -22,12 +27,26 @@ interface SchoolDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertStudent(student: Student)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubject(subject: Subject)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubjectSubjectCrossRef(crossRef: StudentSubjectCrossRef)
+
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String): List<SchoolAndDirector>
 
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
-    suspend fun getSchoolWithStudentWithSchoolName(schoolName: String): List<Student>
+    suspend fun getSchoolWithStudents(schoolName: String): List<SchoolWithStudents>
+
+    @Transaction
+    @Query("SELECT * FROM subject WHERE subjectName = :subjectName")
+    suspend fun getStudentsOfSubject(subjectName: String): List<SubjectWithStudents>
+
+    @Transaction
+    @Query("SELECT * FROM student WHERE studentName = :studentName")
+    suspend fun getSubjectsOfStudent(studentName: String): List<StudentWithSubjects>
 
 }
